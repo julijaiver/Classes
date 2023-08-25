904700b0e07fa822ec0c021da078a6951ecf14fb
julia <julia.ivaskeviciute@gmail.com>
2023-08-25 10:59:05 +0300

    Initial commit

---
+++ first.py
@@ -0,0 +1,28 @@
+intVar = 20
+floatVar = 3.2
+strVar = "Software 1"
+
+print(intVar)
+print(floatVar)
+print(strVar)
+
+# Casting the value from float to int
+result = int(intVar + floatVar)
+print(result)
+
+shareOfLoan = 800.65 / 3
+print(shareOfLoan)
+intShareOfLoan = int(shareOfLoan)
+print(intShareOfLoan)
+
+num1 = int(input("Enter some value:\n "))
+num2 = int(input("Enter a second value:\n "))
+result = num1 + num2
+print('The result is: \t ', result)
+
+name = input("What's your name: ")
+age = input("What's your age: ")
+print("Name: ", name, "Age: ", age)
+print(name, age)
+outPut = f"Name: {name} Age: {age}"
+print(outPut)
---
+++ second.py
@@ -0,0 +1,9 @@
+pi = 3.14159265
+formatPi = "value of Pi: {:.2f}".format(pi)
+print(formatPi)
+
+radius = float(input("Enter the radius: "))
+areaCircle = pi * radius**2
+print (f"Area of circle: {areaCircle:.2f}")
+
+
