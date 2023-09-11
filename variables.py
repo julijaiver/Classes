28a9899a0e997485af24241aa0dc350490b4caaa
julia <julia.ivaskeviciute@gmail.com>
2023-09-11 12:08:41 +0300

    comment

--- variables.py
+++ variables.py
@@ -25,8 +25,23 @@ print(numberSum)
 print(numberProduct)
 print(numberAverage)
 
+# Medieval unit conversion
+talents = float(input("Enter talents:\n "))
+pounds = float(input("Enter pounds:\n"))
+lots = float(input("Enter lots:\n"))
+
+lots_to_grams = (lots * 13.3)
+pounds_to_grams = (13.3 * 32 * pounds)
+talents_to_grams = (13.3 * 32 * 20 * talents)
+
+whole_in_grams = lots_to_grams + pounds_to_grams + talents_to_grams
+print(f"The weight in modern units is:\n {whole_in_grams // 1000} kg and {whole_in_grams % 1000:.3f}")
+
+
 # Random number combinations
 import random
-randomNumber = random.sample(range(0, 9),3)
-print(randomNumber)
+randomThreeDigit = random.sample(range(0, 9),3)
+randomFourDigit = random.sample(range(1, 6), 4)
+print(randomThreeDigit)
+print(randomFourDigit)
 
