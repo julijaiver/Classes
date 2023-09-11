7eb1c5ccb4d1c9fb9013d3363d29ec813f665ec5
julia <julia.ivaskeviciute@gmail.com>
2023-09-11 08:32:27 +0300

    comment

--- Lists_For.py
+++ Lists_For.py
@@ -23,6 +23,20 @@ numbers.sort(reverse=True)
 for i in range(5):
     print(numbers[i])
 
+# Third exercise
+
+inpNumber = int(input("Provide a number: "))
+if inpNumber == 1:
+    print("Not a prime number")
+elif inpNumber > 1:
+    for i in range(2, inpNumber):
+        if inpNumber % i == 0:
+            print(f"{inpNumber} is a not prime number")
+            break
+    else:
+        print(f"{inpNumber} is a prime number")
+
+
 # Fourth testing
 
 cities = []
