67801a0badae077fa81dd38532e093648949f78f
julia <julia.ivaskeviciute@gmail.com>
2023-10-03 20:45:05 +0300

    comment

--- Flight game project/DatabaseForFlight.py
+++ Flight game project/DatabaseForFlight.py
@@ -1,3 +1,5 @@
+import random
+
 import mysql.connector
 
 connection = mysql.connector.connect(
@@ -16,9 +18,9 @@ def get_eu_airports():
 from airport 
 where iso_country in(select iso_country from airport where continent = "EU")
 and ident in(select min(ident) from airport where continent = "EU" 
-and type = "large_airport" or type = "small_airport" group by iso_country)
+and not type = "closed" group by iso_country)
 order by rand()
-limit 40;"""
+limit 50;"""
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -32,7 +34,7 @@ def get_africa_airports():
     and ident in(select min(ident) from airport where continent = "AF" 
     and not type = "closed" group by iso_country)
     order by rand()
-    limit 40;"""
+    limit 50;"""
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -46,18 +48,19 @@ def get_asia_airports():
     and ident in(select min(ident) from airport where continent = "AS" 
     and not type = "closed" group by iso_country)
     order by rand()
-    limit 40;"""
+    limit 50;"""
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
 
 
+# Maybe exclude
 def get_north_am_airports():
     sql = """select iso_country, ident, name, type, latitude_deg, longitude_deg
     from airport 
     where iso_country in(select iso_country from airport where continent = "NA")
-    and ident in(select min(ident) from airport where continent = "NA" 
+    and ident in(select min(ident) from airport where continent = "NA"
     and not type = "closed" group by iso_country)
     order by rand()
     limit 40;"""
@@ -80,13 +83,44 @@ def new_game(player_name, current_airport, battery_power, all_airports):
     sql = "insert into game(screen_name, location, battery_power, score) values (%s, %s, %s, %s);"
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql, (player_name, current_airport, battery_power))
+    game_id = cursor.lastrowid
 
-    #Maybe adding goals
+    # Excluding starting airport
+    game_airport = all_airports[1:].copy()
+    random.shuffle(game_airport)
+
+    #Adding goals
     goals = get_goals()
     goal_list = []
     for goal in goals:
         for i in range(0, goal['probability'], 1):
             goal_list.append(goal['id']
+
+    for i, goal_id in enumerate(goal_list):
+        sql = "insert into spying_location (game, goal, airport) values (%s, %s, %s);"
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute(sql, (game_id, game_airport[i]['ident'], goal_id))
+
+    return game_id
+
+
+# Get airport information
+def get_airport_info(icao):
+    sql =  (f"select country.name, ident, airport.name, latitude_deg, longitude_deg from airport, country "
+            f"where airport.iso_country = country.iso_country and ident = %s;")
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql, icao)
+    result = cursor.fetchall()
+    return result
+
+
+#Set airport as visited
+def airport_visited(game, airport):
+    sql = f"update spying_location set visited = 1 where game = %d and airport = %s;"
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql, (game, airport))
+
+
 
 
 
