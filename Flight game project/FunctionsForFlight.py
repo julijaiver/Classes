850fe67fa699300948f6e41029fc73ccaf6ad79c
julia <julia.ivaskeviciute@gmail.com>
2023-10-04 09:11:15 +0300

    comment

--- Flight game project/FunctionsForFlight.py
+++ Flight game project/FunctionsForFlight.py
@@ -1,5 +1,5 @@
 import random
-
+import
 import mysql.connector
 
 connection = mysql.connector.connect(
@@ -55,7 +55,7 @@ def get_asia_airports():
     return result
 
 
-# Maybe exclude
+# Maybe exclude this one
 def get_north_am_airports():
     sql = """select iso_country, ident, name, type, latitude_deg, longitude_deg
     from airport 
@@ -89,38 +89,59 @@ def new_game(player_name, current_airport, battery_power, all_airports):
     game_airport = all_airports[1:].copy()
     random.shuffle(game_airport)
 
-    #Adding goals
+    # Adding goals
     goals = get_goals()
     goal_list = []
     for goal in goals:
         for i in range(0, goal['probability'], 1):
-            goal_list.append(goal['id']
+            goal_list.append(goal['id'])
 
     for i, goal_id in enumerate(goal_list):
         sql = "insert into spying_location (game, goal, airport) values (%s, %s, %s);"
-            cursor = connection.cursor(dictionary=True)
-            cursor.execute(sql, (game_id, game_airport[i]['ident'], goal_id))
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(sql, (game_id, game_airport[i]['ident'], goal_id))
 
     return game_id
 
 
 # Get airport information
 def get_airport_info(icao):
-    sql =  (f"select country.name, ident, airport.name, latitude_deg, longitude_deg from airport, country "
-            f"where airport.iso_country = country.iso_country and ident = %s;")
+    sql = (f"select country.name, ident, airport.name, latitude_deg, longitude_deg from airport, country "
+           f"where airport.iso_country = country.iso_country and ident = %s;")
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql, icao)
     result = cursor.fetchall()
     return result
 
 
-#Set airport as visited
+# Set airport as visited
 def airport_visited(game, airport):
-    sql = f"update spying_location set visited = 1 where game = %d and airport = %s;"
+    sql = f"update spying_location set visited = 1 where game = %s and airport = %s;"
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql, (game, airport))
 
 
+# What goal is in the location
+def location_goal(game_id, location):
+    sql = (f"select spying_location.id, goal, goal.id as goal, name, points from spying_location "
+           f"inner join goal on goal.id = spying_location.goal where game = %s and airport = %s; ")
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql, (game_id, location))
+    result = cursor.fetchone()
+    return result
+
+# Distance between airports
+def airport_distance
+def location_update(icao, bat_power, score):
+    sql = f"update game set location = %s, battery_power = %s, score = %s where id = %s;"
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql, (icao, bat_power, score))
 
 
+def path_game(path_choice):
+    rand_path = random.randint(1, 5)
+    if path_choice == rand_path:
+        return "Oh no! You were not careful and got caught..."
+    else:
+        return "Path successful! You got 10% extra battery power and 15 points"
 
