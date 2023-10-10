06b9743db623f018fe4eab2585c96ecc98878db6
julia <julia.ivaskeviciute@gmail.com>
2023-10-10 11:49:39 +0300

    comment

--- GameFile.py
+++ GameFile.py
@@ -1,4 +1,5 @@
 import random
+import spyPlaneStory
 from geopy import distance
 
 import mysql.connector
@@ -76,6 +77,17 @@ def airport_visited(game, airport):
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql, (game, airport))
 
+# Check if airport is visited
+def is_visited(game, airport):
+    sql = f"select visited from spying_location where game = %s and airport = %s"
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql, (game, airport))
+    result = cursor.fetchone()
+
+    if result == ({'visited': 1}):
+        return False
+    else:
+        return True
 
 # What goal is in the location
 def location_goal(game_id, location):
@@ -98,12 +110,17 @@ def airport_distance(starting, end):
 
 
 # get airports in range:
-def airports_in_range(icao, airports, remaining_battery):
+def airports_in_range(icao, airports, remaining_battery,game_id):
     in_range = []
-    for airport in airports:
-        distance = airport_distance(icao, airport['ident'])
-        if (distance <= remaining_battery and not distance == 0):
-            in_range.append(airport)
+    for i, airport in enumerate(airports):
+        if(i>=1):
+            distance = airport_distance(icao, airport['ident'])
+            sql = (f"SELECT visited FROM spying_location WHERE game = %s AND airport = %s;")
+            cursor = connection.cursor()
+            cursor.execute(sql,(game_id,airport['ident']))
+            airport_visited = cursor.fetchone()
+            if (distance <= remaining_battery and not distance == 0 and not airport_visited[0] == 1):
+                in_range.append(airport)
     return in_range
 
 
@@ -117,14 +134,38 @@ def location_update(icao, bat_power, score, id):
 # Formulate messages in the main
 def path_game_won(path_choice):
     rand_path = random.randint(1, 5)
-    if path_choice == rand_path:
+    if int(path_choice) == rand_path:
         return False
     else:
         return True
 
 
+# Y or N for the minigame
+def yes_or_no(question: str) -> bool:
+    while True:
+        answer = input(question).upper()
+        if answer in ("Y", "N"):
+            return answer == "Y"
+        print("Invalid input.")
+
+
+# Retrieve the data of top 10 players in the game
+def get_rank():
+    sql = ("SELECT id,screen_name,score FROM game WHERE score >=100 ORDER BY score DESC limit 10;")
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
+
+storyDialog = input('Do you want to read the background story? Y/N: ').upper()
+if storyDialog == 'Y':
+    for line in spyPlaneStory.getStory():
+        print(line)
+
+
 # GAME SETTINGS
-print("When you are ready to start, ")
+print("\nWhen you are ready to start, ")
 player = input("type your name: ")
 # boolean for game over and win
 game_over = False
@@ -155,6 +196,9 @@ current_airport = start_airport
 # game id
 game_id = new_game(player, current_airport, all_airports)
 
+# sets start airport as visited
+airport_visited(game_id, current_airport)
+
 # GAME LOOP
 while not game_over:
     # get current airport info
@@ -172,35 +216,48 @@ while not game_over:
             print("The airport is clear and sunny. You gain 5 points.")
             score = score + 5
         elif goal['goal'] == 2:
-            print("The airport is cloudy. You gain 10 points for your risky actions.")
-            score = score + 10
+            if battery - 50 < 0:
+                print(
+                    f"Oh no, the weather in {airport[0]['name']} looks bad. "
+                    f"Your remaining battery range cannot get you there. You have to do an emergency landing.")
+                battery = 0
+                game_over = True
+                break
+            else:
+                print(
+                    f"The weather in {airport[0]['name']} is Cloudy. You get 10 points,but you spend 50km extra range.")
+                battery = battery - 50
+                score = score + 10
         elif goal['goal'] == 3:
-            user_choice = input("There is a charging point nearby, but this airport seems to be suspicious. Would you like to take the risk,"
-                  "choose a path and try to get to the charging point? If you choose to escape, "
-                                "you can't come back to this airport again\nChoose Y/N: ").upper()
-            if user_choice == "N":
-                print("You did not take the risk, but lost the resources used to travel")
-            elif user_choice == "Y":
-                path_choice = int(input("Choose a path 1-5: "))
+            user_choice = yes_or_no("There is a charging point nearby, but this airport seems to be suspicious.\n "
+                                    "Would you like to take the risk,"
+                                    "choose a path and try to get to the charging point?\n "
+                                    "If you choose to escape, "
+                                    "you can't come back to this airport again\nChoose Y/N: ")
+            if user_choice:
+                path_choice = input('Choose a path 1 - 5: ')
+                while not (path_choice.isdigit() and 1 <= int(path_choice) <= 5):
+                    path_choice = input('Please enter a path # from 1 to 5: ')
                 if path_game_won(path_choice):
                     print("Your path was successful! You got 600 extra battery power and 15 points")
                     battery += 600
                     score += 15
+                    print(f"You now have {battery} km of range in your battery.")
                 else:
                     game_over = True
                     break
-
+            else:
+                print("You did not take the risk, but lost the resources used to travel")
         else:
             print("This airport was expecting you. You have been caught!")
             game_over = True
             break
-        input("Press Enter to continue.")
 
-    # pause
-
+# pause
+    input("Press Enter to continue.")
     # if no battery power, game over
     # show airports in range. if none, game over
-    airports = airports_in_range(current_airport, all_airports, battery)
+    airports = airports_in_range(current_airport, all_airports, battery,game_id)
     print(f"There are {len(airports)} airports in range: ")
     if len(airports) == 0:
         print("You have no more airports in range.")
@@ -208,14 +265,23 @@ while not game_over:
     else:
         print("Airports:")
         for airport in airports:
-            ap_distance = airport_distance(current_airport, airport['ident'])
-            print(f"{airport['name']}, icao: {airport['ident']}, distance: {ap_distance:.0f}km")
+            if is_visited(game_id, airport['ident']):
+                ap_distance = airport_distance(current_airport, airport['ident'])
+                print(f"{airport['name']}, icao: {airport['ident']}, distance: {ap_distance:.0f}km")
+
         # ask for destination
-        dest = input("Enter the ICAO of the destination you would like to go to: ")
+        while True:
+            airport_codes = [dict['ident'] for dict in airports]
+            dest = input("Enter the ICAO of the destination you would like to go to: ").upper()
+            if dest in airport_codes:
+                break
+            else:
+                print("Please enter a correct ICAO.")
         selected_distance = airport_distance(current_airport, dest)
         battery -= selected_distance
         location_update(dest, battery, score, game_id)
         current_airport = dest
+        airport_visited(game_id, current_airport)
         if battery < 0:
             game_over = True
 
@@ -225,5 +291,9 @@ if score >= 100:
 # if game is over the loop stops
 if win:
     print(f"You won! You have gathered {score} points worth of information")
+    win_lists = get_rank()
+    for win_list in win_lists:
+        if game_id == win_list['id']:
+            print(f"Congratulations, you are in the top 10 rank and you rank as No.{win_lists.index(win_list)+1} in the list.")
 else:
     print(f"You lose, you have gathered {score} points worth of information. This is not enough!")
