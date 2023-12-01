276cbf6763838258b07b70b18569ab7314bd1f23
julia <julia.ivaskeviciute@gmail.com>
2023-12-01 09:53:38 +0200

    comment

---
+++ Software2/PROJECT/StartingPop.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const overlay = document.querySelector('#startOverlay');
+const popup = document.querySelector('#startPopContainer');
+const nameForm = document.querySelector('#playerForm');
+
+document.addEventListener('DOMContentLoaded', function () {
+    overlay.style.display = 'block';
+    popup.style.display = 'block';
+});
+
+nameForm.addEventListener('submit', async function(evt) {
+    evt.preventDefault();
+    const name = document.querySelector('#playersName').value;
+
+    //Flask part?
+    try {
+        await fetch('/name', {
+            method: 'POST',
+            body: JSON.stringify({name: name})
+        })
+            .then(response => {
+                if (response.ok) {
+                    overlay.style.display = 'none';
+                    popup.style.display = 'none';
+                } else {
+                    throw new Error('Submission failed.');
+                }
+            })
+    } catch (error) {
+        alert('Error: ' + error.message);
+    }
+});
+
+
+
+// event listener after loading page:
+// overlay and start popup display: block
+//
+// submit button event listener:
+// click, POST data to flask
