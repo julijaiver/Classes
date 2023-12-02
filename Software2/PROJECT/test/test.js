9172600f5ebefb5276a1451f37fdeadb36b47e6a
julia <julia.ivaskeviciute@gmail.com>
2023-12-03 00:22:26 +0200

    comment

--- Software2/PROJECT/test/test.js
+++ Software2/PROJECT/test/test.js
@@ -2,67 +2,75 @@
 
 // global variables
 const overlay = document.querySelector('.overlay');
-
+const popup = document.querySelector('.popup');
 function initializeMap() {
-const map = L.map('map').setView([60, 24], 7);
+    const map = L.map('map').setView([51.505, -0.09], 7);
 
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    maxZoom: 20,
-}).addTo(map);
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        maxZoom: 20,
+    }).addTo(map);
 
-const redMarker = L.icon({
-    iconUrl: 'photos/roundMarker.png',
-    iconSize: [50, 50],
-    iconAnchor: [25, 20]
-});
+    const redMarker = L.icon({
+        iconUrl: 'photos/roundMarker.png',
+        iconSize: [50, 50],
+        iconAnchor: [25, 20]
+    });
 
-const greenMarker = L.icon({
-    iconUrl: 'photos/marker2.png',
-    iconSize: [50, 50],
-    iconAnchor: [25, 20]
-});
+    const greenMarker = L.icon({
+        iconUrl: 'photos/marker2.png',
+        iconSize: [50, 50],
+        iconAnchor: [25, 20]
+    });
 
-
-
-// for (let airport of gameData.location) {
     let active = false;
     let marker;
-     if (active) { //if current location
-         marker = L.marker([51.505, -0.09]).addTo(map);
-         marker.bindPopup(`You are here: <br>//airport name</br>`);
-         marker.openPopup();
-         marker.setIcon(greenMarker);
-    } else { // to every other location
-         marker = L.marker([51.505, -0.09]).addTo(map);
-         marker.setIcon(redMarker);
-         const popupContent = document.createElement('div');
-         const h4 = document.createElement('h4');
-         h4.innerHTML = `//airport name`;
-         popupContent.append(h4);
-         const p = document.createElement('p');
-         p.innerHTML = `Distance: //range`;
-         popupContent.append(p);
-         const flyButton = document.createElement('button');
-         flyButton.classList.add('popup-button');
-         flyButton.innerHTML = 'FLY';
-         popupContent.append(flyButton);
-         marker.bindPopup(popupContent, {
-             maxHeight: 130
-         });
-     }
-     marker.on('mouseover', function () {
-         marker.openPopup();
-     });
-     marker.on('mouseout', function () {
-         marker.closePopup();       //Popup doesn't stay open when mouse is over it
-     });
 
-// }
+    if (active) {
+        marker = L.marker([51.505, -0.09], {icon: greenMarker}).addTo(map);
+        marker.bindPopup(`You are here: <br>//airport name</br>`);
+        marker.openPopup();
+    } else {
+        marker = L.marker([51.505, -0.09], {icon: redMarker}).addTo(map);
+        const popupContent = document.createElement('div');
+        const h4 = document.createElement('h4');
+        h4.innerHTML = `//airport name`;
+        popupContent.append(h4);
+        const p = document.createElement('p');
+        p.innerHTML = `Distance: //range`;
+        popupContent.append(p);
+        const flyButton = document.createElement('button');
+        flyButton.classList.add('popup-button');
+        flyButton.innerHTML = 'FLY';
+        popupContent.append(flyButton);
+        marker.bindPopup(popupContent);
+    }
+
+        // Function to close the popup
+    function closeMarkerPopup() {
+        if (!map.getBounds().contains(marker.getLatLng())) {
+            marker.closePopup();
+        }
+    }
+
+    // Event listeners for the marker and popup
+    marker.on('mouseover', function () {
+        marker.openPopup();
+    });
+
+    marker.on('mouseout', function () {
+        closeMarkerPopup();
+    });
+
+    marker.getPopup().on('mouseout', function () {
+        closeMarkerPopup();
+    });
+}
+
+//Popup doesn't stay open when mouse is over it
+
 
 // Need to set initial coordinates with database random loc.
 //const map = L.map('map').setView([51.505, -0.09], 13);
-
-}
 
 function showPopup(id) {
     const popup = document.querySelector(`#${id}`);
@@ -75,6 +83,7 @@ async function nameFormSubmit(evt) {
     const name = document.querySelector('#playersName').value;
     const nameDisplay = document.querySelector('#displayName');
     nameDisplay.append(name);
+    popup.style.display = 'none';
      //Flask part?
 //     try {
 //         await fetch('/name', {
@@ -106,7 +115,43 @@ document.addEventListener('DOMContentLoaded', function () {
 const nameForm = document.querySelector('#playerForm');
 nameForm.addEventListener('submit', nameFormSubmit);
 
+
     // in the html file display name
+const submitDifficultyButton = document.querySelector('#submitContinent');
+//submitDifficultyButton.addEventListener('click', async function() {
+//     const selectedContinent = document.querySelector('input[name="Difficulty"]:checked').id;
+//     const data = {
+//         continent: selectedContinent
+//     };
+//
+//     fetch('/submit_continent', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     })
+//     .then(response => {
+//         if (response.ok) {
+//             return response.json();
+//         } else {
+//             throw new Error('Response failed');
+//         }
+//     })
+//         .then(aiportData => {
+//             //handle airport data
+//             airportData.forEach(airport => {
+//                 //do something
+//             })
+//         })
+//         .catch(error => {
+//             console.error('Error: ', error)
+//         });
+// });
+// })
+
+
+
 
 //showPopup('missionSuccess');
 //success button.eventlistener. when clicked game starts again (if points == leaderboard then show)
